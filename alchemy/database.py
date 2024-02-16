6aaffcce1ad7065545a258a2c2107b85807b42e8
root <root@Potato>
2024-02-15 18:24:22 -0600

    db add retry

--- alchemy/database.py
+++ alchemy/database.py
@@ -1,6 +1,7 @@
 from alchemy.models import Base, DBMatch, DBPlayer, PlayerKills, PlayerDeaths, WeaponKills, WeaponDeaths
 from sqlalchemy import engine, create_engine
 from sqlalchemy.orm import Session
+from tenacity import retry, stop_after_attempt
 
 
 engine = create_engine("sqlite:///the_line.db", echo=False)
@@ -8,13 +9,14 @@ engine = create_engine("sqlite:///the_line.db", echo=False)
 print("<<<< CREATING TABLES >>>>")
 Base.metadata.create_all(bind=engine)
 
-
+@retry(stop=stop_after_attempt(5))
 def add_data(data):
 	with Session(engine) as session, session.begin():
 		try:
 			session.add_all(data)
 		except:
 			session.rollback()
+			raise Exception
 
 
 def in_db(obj, primary_key):
