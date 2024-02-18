eef060344262daa4dcf86a448703426b8cc77be2
ethan <e.timothy65@gmail.com>
2024-02-17 19:06:33 -0600

    fixed data parsing, add top weapons start

--- alchemy/database.py
+++ alchemy/database.py
@@ -8,6 +8,7 @@ from tenacity import retry, stop_after_attempt
 engine = create_engine("sqlite:///the_line.db", echo=False)
 Base.metadata.create_all(bind=engine)
 
+
 @retry(stop=stop_after_attempt(5))
 def add_data(data):
 	with Session(engine) as session, session.begin():
@@ -25,9 +26,31 @@ def in_db(obj, primary_key):
 		if session.query(obj).get(primary_key):
 			return True
 
+
+def get_weapons():
+	with Session(engine) as session:
+		stmt = (
+			select(WeaponKills)
+			.join(WeaponKills.player)
+		)
+
+		weapons = []
+
+		result = session.execute(stmt).scalars()
+		for w in result:
+			w_name = w.weapons
+			if w_name not in weapon:
+				weapons.append(w_name)
+				print(w.player_name, w.weapon)
+
+
 def get_player_by_steam64(steam_64):
 	with Session(engine) as session:
-		query = select(DBPlayer).join(DBPlayer.match).where(DBPlayer.steam_id_64 == steam_64)
+		query = (
+			select(DBPlayer)
+			.join(DBPlayer.match)
+			.where(DBPlayer.steam_id_64 == steam_64)
+		).order_by(DBPlayer.kills.desc())
 
 		result = session.execute(query).scalars()
 		players = []
@@ -36,6 +59,42 @@ def get_player_by_steam64(steam_64):
 			players.append(player)
 
 		return players
+
+
+def get_top_killers():
+	delta = datetime.datetime.now() - datetime.timedelta(days=7)
+
+	with Session(engine) as session:
+		u = union_all(
+			select(DBPlayer)
+			.join(DBPlayer.match)
+			.where(DBMatch.server_name == 'west'),
+			select(DBPlayer)
+			.join(DBPlayer.match)
+			.where(DBMatch.server_name == 'east'),
+			select(DBPlayer)
+			.join(DBPlayer.match)
+			.where(DBMatch.server_name == 'central')
+		).order_by(DBPlayer.kills.desc())
+
+		stmt = select(DBPlayer).from_statement(u)
+		players = session.execute(stmt).scalars()
+		top_players = []
+		player_64s = []
+
+		for player in players:
+			if (len(top_players) >= 10):
+				break
+			elif player.steam_id_64 in player_64s:
+				print(player.player, player.kills)
+				continue
+			else:
+				print(player.player, player.kills)
+				top_players.append(player)
+				player_64s.append(player.steam_id_64)
+
+		return top_players
+
 
 def get_top_week_killers():
 	delta = datetime.datetime.now() - datetime.timedelta(days=7)
@@ -58,7 +117,7 @@ def get_top_week_killers():
 		player_64s = []
 
 		for player in players:
-			if (len(top_players) >= 10):
+			if (len(top_players) >= 100):
 				break
 			elif player.steam_id_64 in player_64s:
 				continue
--- provider.py
+++ provider.py
@@ -1,10 +1,26 @@
-from alchemy.database import get_player_by_steam64, get_top_week_killers
+from alchemy.database import get_player_by_steam64, get_top_killers, get_top_week_killers
 
 
 class Provider:
 
     def __init__(self):
     	pass
+
+
+    def top_killers(self):
+        players = get_top_killers()
+        ret_players = []
+
+        for player in players:
+            data = dict([
+                ('steam_64', player.steam_id_64),
+                ('name', player.player),
+                ('kills', player.kills),
+            ])
+            ret_players.append(data)
+
+        return ret_players
+
 
     def week_top_killers(self):
         players = get_top_week_killers()
